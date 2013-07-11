9ea1279ce2757959f095b9d659c609743ce45248
Michael Robbins <michael@dalmura.com.au>
2013-07-11 23:12:41 +1000

    Next round of changes

--- jobqueue_daemon.py
+++ jobqueue_daemon.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-from jobqueue_manager.manager import JobQueueManager
+from manager.manager import JobQueueManager
 
 parser = argparse.ArgumentParser(
         description='Daemon for the Media Server Job Processor'
